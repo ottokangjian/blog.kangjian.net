ffeaa7659bcbf7f30f4539fa5dc3f0341895f9c7
ottokangjian <kangjiankangjian@gmail.com>
2017-05-29 15:54:49 +0800

    2017-05-29

--- d.sh
+++ d.sh
@@ -2,4 +2,3 @@ rsync -avu --progress --delete /Users/jiankang/OneDrive/images.kangjian.net/ roo
 rsync -avu --progress --delete /Users/jiankang/OneDrive/download.kangjian.net/ root@173.230.149.243:/data/wwwroot/kangjian.net/download/
 hexo g
 hexo d
-
