328069eb7e10c99ff37a3506c7fd2246c7828b06
ottokangjian <kangjiankangjian@gmail.com>
2020-03-13 19:18:23 +0800

    update

--- up.sh
+++ up.sh
@@ -1,3 +1,2 @@
-rsync -avu --progress --delete /Users/jiankang/OneDrive/Documents/images.kangjian.net/
-root@173.230.149.243:/data/wwwroot/kangjian.net/images/
+rsync -avu --progress --delete /Users/jiankang/OneDrive/Documents/images.kangjian.net/ root@173.230.149.243:/data/wwwroot/kangjian.net/images/
 rsync -avu --progress --delete /Users/jiankang/OneDrive/Documents/download.kangjian.net/ root@173.230.149.243:/data/wwwroot/kangjian.net/download/
